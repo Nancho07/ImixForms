36d450354d4b933ebf992e1ea43360fe9eb301db
Nancho07 <hernan@imixdigital.com>
2017-08-11 23:11:37 -0600

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -2122,19 +2122,19 @@ function onAppReady(){
         $("#accordion").empty();
         $.each( categorias, function( key, value ) {
             i=i+1;
-            respu = '<div class="panel panel-default col-xs-12 col-md-12 col-lg-12">'+
-                        '<div id="'+i+'" class="caja">'+
-                          '<div class="panel-heading menu" id="catego'+value.catego_k+'" style="background-color:'+value.catego_color+';">'+
+            respu = '<!--<div class="panel panel-default col-xs-12 col-md-12 col-lg-12">-->'+
+                        '<div id="'+i+'" class="caja  col-xs-12 col-md-12 col-lg-12">'+
+                          '<div class="menu" id="catego'+value.catego_k+'" style="background-color:'+value.catego_color+';"><!-- panel-heading  -->'+
                             '<h4 class="panel-title ">'+
-                              '<a data-toggle="collapse"  style="color:'+value.catego_text_color+'; text-shadow:none;">'+value.nombre+'</a>'+ /*data-parent="#accordion" href="#collapse'+value.catego_k+'"*/
+                              '<a style="color:'+value.catego_text_color+'; text-shadow:none;">'+value.nombre+'</a><!-- data-toggle="collapse" -->'+
                             '</h4>'+
                           '</div>'+
-                          '<div id="collapse'+value.catego_k+'" class="panel-collapse collapse in">'+
+                          '<div id="collapse'+value.catego_k+'" ><!-- class="panel-collapse collapse in" -->'+
                               '<div class="note note-catego" id="nota'+value.catego_k+'">'+                                
                               '</div>'+                          
                           '</div>'+
                         '</div>'+
-                     '</div>';
+                     '<!--</div>-->';
             $("#accordion").append(respu);
             $("#catego"+value.catego_k).bind("click",function(){
                 var configCatego={};
